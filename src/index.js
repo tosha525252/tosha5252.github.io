7351d64bd91258447b47aa89dcc19e1d8acbd9dd
Natosha Caldwell <tosha5252@gmail.com>
2026-06-02 17:18:59 -0500

    Wrap App component with HashRouter for routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { HashRouter } from 'react-router-dom';
+
+<HashRouter>
+  <App />
+</HashRouter>
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +15,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
