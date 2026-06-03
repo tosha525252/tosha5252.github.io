82802d1f3c3637cfb77b59b4c4a62102a3f148d5
Natosha Caldwell <tosha5252@gmail.com>
2026-06-03 14:22:15 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,31 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from 'react-router-dom';
-
-// Source - https://stackoverflow.com/a/54428366
-// Posted by varoons
-// Retrieved 2026-06-02, License - CC BY-SA 4.0
-
-import BrowserRouter from 'react-router-dom/BrowserRouter'
-
-ReactDOM.render((
-  <BrowserRouter basename="/tosha5252.github.io">
-  <App />
-</BrowserRouter>
-), ...)  
-
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
